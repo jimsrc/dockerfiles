21dea44bd96c30611559845a476b8ee649469389
jimarch <jimmy.ilws@gmail.com>
2018-03-07 23:47:10 -0300

    [hegea] install python and the same libraries with the SAME versions as in the 'hegea' server

---
+++ ubuntus/hegea_conda/conda/conda_installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# the Anaconda/Miniconda installer is interactive, so this is
+# a programatic version of that installation
+
+# installer executable
+EXE=$1          # 1st argument
+# where to install Anaconda
+CondaDir=$2     # 2nd argument
+
+
+# non-interactive installer of Miniconda
+$EXE 1> /dev/null <<-EOF
+q
+yes
+$CondaDir
+EOF
+
+
+#EOF
+
